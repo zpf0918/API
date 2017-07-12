435fdc3e360ff0c84fbdd4dce47c8006c3f5fd06
zpf0918 <zhanggongzifei@163.com>
2017-07-12 12:55:42 +0800

    implement 3-8

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,7 +2,16 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  SEATS = begin
+    (1..6).to_a.map do |series|
+      ["A", "B", "C"].map do |letter|
+        "#{series}#{letter}"
+      end
+    end
+  end.flatten
+
   def available_seats
     ["1A","1B","1C","1D","1F"]
+    return SEATs - self.reservations.pluck(:seat_number)
   end
 end
