58df40c230653b7642f4ccec448f2c8d74a482a5
zpf0918 <zhanggongzifei@163.com>
2017-07-12 10:44:59 +0800

    implement 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, :default => { :format => :json} do
+    namespace :v1 do
+      get "/trains" => "trains#index", as: :trains
+      get "/trains/:train_number" => "trains#show", as: :train
+      get "/reservations/:booking_code" => "reservations#show", as: :revervation
+      post "/reservations" => "reservations#create", as: :create_reservations
+      patch "reservations/:booking_code" => "reservations#update", as: :update_reservation
+      delete "reservations/:booking_code" => "reservations#destroy", as: :destroy_reservation
+    end
+  end
 end
